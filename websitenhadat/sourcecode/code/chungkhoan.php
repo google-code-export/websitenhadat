b039f1a0e33dc36337d9398a9a9532cc99cb8336
cnkhanhdl <cnkhanhdl@11ca2b12-d4aa-3335-f2e9-71d33cccbe8c>
2010-11-14 17:22:49 +0000

    import file chứng khoán

---
+++ websitenhadat/sourcecode/code/chungkhoan.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style>
+body
+{
+font-family:Arial;
+font-size:11px;
+}
+.borderpicLR {
+	border-top-width: 0px;
+	border-right-width: 1px;
+	border-bottom-width: 0px;
+	border-left-width: 1px;
+	border-top-style: none;
+	border-right-style: solid;
+	border-bottom-style: none;
+	border-left-style: solid;
+	border-right-color: #e6dfdf;
+	border-left-color: #e6dfdf;
+}
+.bTitle {
+	font-family: Tahoma;
+	font-size: 15px;
+	font-weight: bold;
+	color: #FFFFFF;
+	line-height: normal;
+	text-decoration: none;
+	text-transform: none;
+}
+.text {
+	line-height: 17px;
+	font-family: Tahoma, Arial, Verdana;
+	font-size: 11px;
+	color: #000000;
+	text-decoration: none;
+	font-weight: normal;
+	}
+</style>
+</head>
+
+<body>
+<table border="0" cellspacing="0" cellpadding="0">
+<tr>
+                                              <td>&nbsp;</td>
+  </tr>
+                                            <tr>
+                                              <td><img src="images/gold_price_top.jpg" width="189" height="14"></td>
+                                            </tr>
+                                            <tr>
+                                              <td background="images/gold_price.jpg" class="borderpicLR" style="background-repeat:no-repeat"><table border="0" cellpadding="0" cellspacing="0">
+                  <tr>
+                                                    <td rowspan="4">&nbsp;</td>
+                                            <td colspan="2"><span class="bTitle"> Chứng khoán</span></td>
+                                                    
+                                                </tr>
+                                                  <tr>
+                                                    <td height="5"><table width="83%" height="20" cellpadding="0" cellspacing="0">
+                                                        <tbody>
+                                                          <tr height="20">
+                                                            <td nowrap="nowrap" align="center" class="text">Mã CK</td>
+                                                            <td nowrap="nowrap" align="center" class="text">Giá 1 </td>
+                                                            <td nowrap="nowrap" align="center" class="text">Giá 2</td>
+                                                            <td nowrap="nowrap" align="center" class="text">số lượng</td>
+                                                          </tr>
+                                                        </tbody>
+                                                    </table></td>
+                                                  </tr>
+
+                                                  <tr>
+                                                    <td><iframe frameborder="0" allowtransparency="true" src="http://vnexpress.net/User/ck/hcms/HCMStockSmall.asp" scrolling="auto"  marginwidth="0" height="150px" width="175px"> </iframe></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td height="5" colspan="2"></td>
+                                                  </tr>
+                                              </table></td>
+  </tr>
+                                            <tr>
+                                              <td><img src="images/bottom_left.jpg" width="189" height="5"></td>
+                                            </tr>
+											<tr><td>&nbsp;</td>
+											</tr>
+										<!-- BEGIN GlobalLeftLogoRow -->
+											<tr>
+											  <td></td>
+											</tr>
+											<tr><td>&nbsp;</td></tr>
+										 <!-- END GlobalLeftLogoRow -->   
+</table>
+</body>
+</html>
