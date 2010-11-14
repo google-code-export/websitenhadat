7c19e155e06891d88cf33c73a6b71dee2c64a679
cnkhanhdl <cnkhanhdl@11ca2b12-d4aa-3335-f2e9-71d33cccbe8c>
2010-11-14 17:22:19 +0000

    import trang chung khoan , ngoai te .

---
+++ websitenhadat/sourcecode/code/ngoaite.php
@@ -0,0 +1,33 @@
+<html>  
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+<style> 
+Body{font-size:12px; font:Tahoma}
+TABLE {
+	PADDING-RIGHT: 0px; PADDING-LEFT: 0px; PADDING-BOTTOM: 0px; PADDING-TOP: 0px; BORDER-COLLAPSE: collapse; border-spacing: 0
+}
+.tbl-01 {
+BORDER-RIGHT: #ccc 1px solid; BORDER-TOP: #ccc 1px solid; FONT-SIZE: 12px; MARGIN: 10px 0px; 
+BORDER-LEFT: #ccc 1px solid; WIDTH: 200px; BORDER-BOTTOM: #ccc 1px solid;
+}
+.tbl-01 TR.imp {
+    FONT-WEIGHT: bold; 
+}
+.tbl-01 TH {
+    COLOR: #333333; BACKGROUND-COLOR: #88ee99
+}
+.tbl-01 TH {
+    PADDING-RIGHT: 4px; PADDING-LEFT: 4px; PADDING-BOTTOM: 3px; PADDING-TOP: 3px
+}
+.tbl-01 TD {
+    PADDING-RIGHT: 4px; PADDING-LEFT: 4px; PADDING-BOTTOM: 3px; PADDING-TOP: 3px
+}
+.tbl-01 TD {
+    VERTICAL-ALIGN: top; 
+}}
+</style> 
+<body style="margin:0px 0px 0px 0px"> 
+<table class=tbl-01 border="0" width="200">
+  <tbody><tr><th width="27%" >Mã NT</th>
+<th width="38%" >Mua</th>
+<th width="35%" >Bán</th>
+</tr><tr ><td>AUD</td><td>17,151.22</td><td>17,507.83</td></tr><tr style='BACKGROUND-COLOR: #ddd'><td>CAD</td><td>18,236.30</td><td>18,634.52</td></tr><tr ><td>CHF</td><td>18,486.37</td><td>18,889.75</td></tr><tr style='BACKGROUND-COLOR: #ddd'><td>EUR</td><td>24,441.51</td><td>24,824.93</td></tr><tr ><td>GBP</td><td>29,857.57</td><td>30,448.12</td></tr><tr style='BACKGROUND-COLOR: #ddd'><td>HKD</td><td>2,478.12</td><td>2,527.14</td></tr><tr ><td>JPY</td><td>225.47</td><td>231.09</td></tr><tr style='BACKGROUND-COLOR: #ddd'><td>SGD</td><td>14,197.31</td><td>14,478.11</td></tr><tr ><td>THB</td><td>607.49</td><td>633.90</td></tr><tr style='BACKGROUND-COLOR: #ddd'><td>USD</td><td>19,475.00</td><td>19,500.00</td></tr></tbody></table>
