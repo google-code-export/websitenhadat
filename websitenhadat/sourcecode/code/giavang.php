04cae5a9d087c99f42cdb7cff2f7dfd11b7d9036
cnkhanhdl <cnkhanhdl@11ca2b12-d4aa-3335-f2e9-71d33cccbe8c>
2010-11-14 17:24:28 +0000

    import module giá vàng

---
+++ websitenhadat/sourcecode/code/giavang.php
@@ -0,0 +1,59 @@
+<html> 
+<title>10S.VN - TIM KIEM THONG TIN</title> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+</head> 
+<body style="margin:0px 0px 0px 0px"> 
+<!--<h3>Trang web dang nang cap. Quy khach vui long quay lai sau.</h3>--> 
+ 
+ 
+<table width="200" border='0' cellspacing='0' cellpadding='2'><tr><td width='41%' height='22' align='center' bgcolor='#88ee99' class='text_tgvang_mota'><strong>    Loại</strong></td>
+<td width='24%' align='center' bgcolor='#88ee99' class='text_tgvang_mota'><strong>Mua</strong></td>
+<td width='31%' align='center' bgcolor='#88ee99' class='text_tgvang_mota'><strong>Bán</strong></td>
+</tr><tr> 
+						<td width='41%' align='right' class='text_tgvang_chitiet'><strong> SJC 1 Kg</strong> &nbsp;</td> 
+	  <td width='24%' align='right' class='text_tgvang_chitiet'>28.600</td> 
+	  <td width='31%' align='right' class='text_tgvang_chitiet'>28.640&nbsp;</td> 
+	  
+						</tr><tr> 
+						<td width='41%' align='right' class='text_tgvang_chitiet'><strong> SJC 10L</strong> &nbsp;</td> 
+						<td width='24%' align='right' class='text_tgvang_chitiet'>28.600</td> 
+						<td width='31%' align='right' class='text_tgvang_chitiet'>28.640&nbsp;</td> 
+						
+						</tr><tr> 
+<td width='41%' align='right' class='text_tgvang_chitiet'><strong> SJC 1L</strong> &nbsp;</td> 
+<td width='24%' align='right' class='text_tgvang_chitiet'>28.600</td> 
+<td width='31%' align='right' class='text_tgvang_chitiet'>28.640&nbsp;</td> 
+
+</tr><tr> 
+						<td width='41%' align='right' class='text_tgvang_chitiet'><strong> SJC 5c</strong> &nbsp;</td> 
+	  <td width='24%' align='right' class='text_tgvang_chitiet'>28.600</td> 
+	  <td width='31%' align='right' class='text_tgvang_chitiet'>28.660&nbsp;</td> 
+	  
+						</tr><tr> 
+						<td width='41%' align='right' class='text_tgvang_chitiet'><strong> SJC 2c</strong> &nbsp;</td> 
+						<td width='24%' align='right' class='text_tgvang_chitiet'>28.600</td> 
+						<td width='31%' align='right' class='text_tgvang_chitiet'>28.670&nbsp;</td> 
+						
+						</tr><tr> 
+						<td width='41%' align='right' class='text_tgvang_chitiet'><strong> SJC 1c</strong> &nbsp;</td> 
+						<td width='24%' align='right' class='text_tgvang_chitiet'>28.600</td> 
+						<td width='31%' align='right' class='text_tgvang_chitiet'>28.670&nbsp;</td> 
+					
+						</tr><tr> 
+<td width='41%' align='right' class='text_tgvang_chitiet'><strong> 24 K</strong> &nbsp;</td> 
+<td width='24%' align='right' class='text_tgvang_chitiet'>28.140</td> 
+<td width='31%' align='right' class='text_tgvang_chitiet'>28.640&nbsp;</td> 
+ 
+</tr><tr> 
+<td width='41%' align='right' class='text_tgvang_chitiet'><strong> 18 K</strong> &nbsp;</td> 
+<td width='24%' align='right' class='text_tgvang_chitiet'>20.830</td> 
+<td width='31%' align='right' class='text_tgvang_chitiet'>21.630&nbsp;</td> 
+
+</tr><tr> 
+<td width='41%' align='right' class='text_tgvang_chitiet'><strong> 14 K</strong> &nbsp;</td> 
+<td width='24%' align='right' class='text_tgvang_chitiet'>16.050</td> 
+<td width='31%' align='right' class='text_tgvang_chitiet'>16.850&nbsp;</td> 
+
+</tr></table>
+
