4edb1e76098968cb78bb7552465eab8c84f340fa
cnkhanhdl <cnkhanhdl@11ca2b12-d4aa-3335-f2e9-71d33cccbe8c>
2010-11-15 15:08:02 +0000

    trang đăng nhập , đăng kí.

---
+++ websitenhadat/sourcecode/admin/login_error.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="css/link.css" rel="stylesheet" type="text/css" />
+<link href="css/Trangtri.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div align="center" class="chu1Copy">Đăng Nhập Lỗi!</div>
+
+<div align="center"><span class="lienket"><a href="index.php">Trở Lại</a></span></div>
+</body>
+
+</html>
