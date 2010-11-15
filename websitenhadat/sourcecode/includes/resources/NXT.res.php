dc1a824b6673f71b834f200e1c8944dd3f48601e
cnkhanhdl <cnkhanhdl@11ca2b12-d4aa-3335-f2e9-71d33cccbe8c>
2010-11-15 16:05:32 +0000

    import phần hổ trợ để làm tool trong dreamwear.

---
+++ websitenhadat/sourcecode/includes/resources/NXT.res.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * ADOBE SYSTEMS INCORPORATED
+ * Copyright 2007 Adobe Systems Incorporated
+ * All Rights Reserved
+ * 
+ * NOTICE:  Adobe permits you to use, modify, and distribute this file in accordance with the 
+ * terms of the Adobe license agreement accompanying it. If you have received this file from a 
+ * source other than Adobe, then your use, modification, or distribution of it requires the prior 
+ * written permission of Adobe.
+ */
+
+$res = array(
+'Insert_FH' => 'Thêm',
+'Update_FH' => 'Cập Nhật',
+'Record_FH' => 'Record',
+'Insert_FB' => 'Thêm',
+'Update_FB' => 'Cập Nhật',
+'Delete_FB' => 'Xóa',
+'Cancel_FB' => 'Trở Lại',
+'Insert as new_FB' => 'Thêm Mới',
+'Select one...' => 'Chọn...',
+'Are you sure?' => 'Bạn có chắc không?',
+'Filter' => 'Lọc Dữ Liệu',
+'Show' => 'Hiện Dữ Liệ',
+'all' => 'Tất Cả',
+'records' => 'Dữ Liệu',
+'Reset filter' => 'Trở Lại',
+'Show filter' => 'Hiện DL',
+'Order' => 'Sắp Xếp',
+'save' => 'Lưu',
+'None' => 'Không',
+'The table is empty or the filter you\'ve selected is too restrictive.' => 'The table is empty or the filter you\'ve selected is too restrictive.',
+'edit_one' => 'Sửa',
+'delete_one' => 'Xóa',
+'edit_all' => 'Sửa',
+'delete_all' => 'Xóa',
+'add new' => 'Thêm Mới',
+'ARE_YOU_SURE_MOVE' => 'You changed the order of some records and did not save the changes. If you continue, the changes will be lost.',
+'ARE_YOU_SURE_DELETE' => 'Bạn có chắc muốn xóa dòng này?',
+'PLEASE_SELECT_RECORD' => 'Làm ơn chọn ít nhất 1 dòng',
+'to' => 'đến',
+'of' => 'của',
+'First' => 'Đầu tiên',
+'Previous' => 'Trở lại',
+'Next' => 'Tiếp',
+'Last' => 'Sau',
+);
+?>
